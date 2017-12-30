67d6d8efec49283e1e7382c4a5584e686441300e
高禎駿 <b02705050@ntu.edu.tw>
2017-12-31 00:16:46 +0800

    d

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function ChangeDiscriptionFontSize() {
-    var discription_length = 1;
+    var discriptions = document.getElementsByClassName("discription");
+    string_length = discriptions[0].innerHTML.length;
+
+    if (string_length > 6 && string_length < 100) {
+        document.getElementsByClassName("discription")[0].innerHTML = '<font size = 40px>' + discriptions[0].innerHTML + '</font>'
+    }
 }
