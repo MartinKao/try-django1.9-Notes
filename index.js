8c01b461c92de854f06a56b0b8b3e3e0a650f1f8
高禎駿 <b02705050@ntu.edu.tw>
2018-01-13 01:38:58 +0800

    index.js debug

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function ChangeDiscriptionFontSize() {
     var discriptions = document.getElementsByClassName("discription");
     string_length = discriptions[0].innerHTML.length;
 
-    if (string_length > 6 && string_length < 100) {
-        document.getElementsByClassName("discription")[0].innerHTML = '<font size = 40px>' + discriptions[0].innerHTML + '</font>'
+    if (string_length > 6 && string_length < 50) {
+        document.getElementsByClassName("discription")[0].innerHTML = '<span style="font-size:40px;">' + discriptions[0].innerHTML + '</span>'
+    }else if(string_length >= 50) {
+        document.getElementsByClassName("discription")[0].innerHTML = '<span style="font-size:30px;">' + discriptions[0].innerHTML + '</span>'
     }
 }
